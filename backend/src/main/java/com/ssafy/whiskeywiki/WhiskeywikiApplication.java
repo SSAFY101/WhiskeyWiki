2bee66748701ffa82b00f649e310a7aac5442924
조담현 <j38903639@gmail.com>
2024-03-13 11:06:03 +0900

    Feat: 패키지 구조 생성

---
+++ backend/src/main/java/com/ssafy/whiskeywiki/WhiskeywikiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.whiskeywiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeywikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeywikiApplication.class, args);
+	}
+
+}
