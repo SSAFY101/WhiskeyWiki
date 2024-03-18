5a8d135efe4cd5f3f7dbb3c1aeaebbd7ee008991
yuncheol-AHN <anchul@hanmail.net>
2024-03-18 20:47:41 +0900

    Fix: user entity의 onToMany 수정, ownWhiskeyList 추가

--- backend/src/main/java/com/ssafy/whiskeywiki/domain/user/domain/User.java
+++ backend/src/main/java/com/ssafy/whiskeywiki/domain/user/domain/User.java
@@ -44,29 +44,32 @@ public class User {
     //연관관계 매핑//
 
     //1. 리뷰와 일대다
-    @OneToMany(mappedBy = "reviews")
+    @OneToMany(mappedBy = "user")
     @Builder.Default
     private List<Review> reviewList = new ArrayList<>();
 
     //2. 위스키 보유와 일대다
+    @OneToMany(mappedBy = "user")
+    @Builder.Default
+    private List<OwnWhiskey> ownWhiskeyList = new ArrayList<>();
 
     //3. 즐겨찾기와 일대다
-    @OneToMany(mappedBy = "favorites")
+    @OneToMany(mappedBy = "user")
     @Builder.Default
     private List<Favorite> favoriteList = new ArrayList<>();
 
     //4. 채팅과 일대다
-    @OneToMany(mappedBy = "chats")
+    @OneToMany(mappedBy = "user")
     @Builder.Default
     private List<Chat> chatList = new ArrayList<>();
 
     //5. 유저 채팅방(User_ChatRooms)과 일대다
-    @OneToMany(mappedBy = "userChatRooms")
+    @OneToMany(mappedBy = "user")
     @Builder.Default
     private List<userChatRooms> userChatRoomsList = new ArrayList<>();
 
     //6. 거래상태와 일대다
-    @OneToMany(mappedBy = "tradeStatus")
+    @OneToMany(mappedBy = "user")
     @Builder.Default
     private List<TradeStatus> tradeStatusList = new ArrayList<>();
 
