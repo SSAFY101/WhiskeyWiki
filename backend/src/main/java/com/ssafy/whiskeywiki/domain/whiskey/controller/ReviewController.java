4c0febd4613b2ea9076143e5c3ec5c9ee1ef31e3
조담현 <j38903639@gmail.com>
2024-03-19 17:55:54 +0900

    Feat: yml 설정파일 분리

--- backend/src/main/java/com/ssafy/whiskeywiki/domain/whiskey/controller/ReviewController.java
+++ backend/src/main/java/com/ssafy/whiskeywiki/domain/whiskey/controller/ReviewController.java
@@ -1,4 +1,15 @@
 package com.ssafy.whiskeywiki.domain.whiskey.controller;
 
+import com.ssafy.whiskeywiki.domain.whiskey.service.ReviewService;
+import com.ssafy.whiskeywiki.domain.whiskey.service.WhiskeyInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/review")
 public class ReviewController {
+
+    private final ReviewService reviewService;
 }
--- backend/src/main/java/com/ssafy/whiskeywiki/domain/whiskey/controller/WhiskeyInfoController.java
+++ backend/src/main/java/com/ssafy/whiskeywiki/domain/whiskey/controller/WhiskeyInfoController.java
@@ -1,4 +1,18 @@
 package com.ssafy.whiskeywiki.domain.whiskey.controller;
 
+
+import com.ssafy.whiskeywiki.domain.whiskey.domain.Whiskey;
+import com.ssafy.whiskeywiki.domain.whiskey.service.WhiskeyInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/whiskeyinfo")
 public class WhiskeyInfoController {
+
+    private final WhiskeyInfoService whiskeyInfoService;
+
+
 }
