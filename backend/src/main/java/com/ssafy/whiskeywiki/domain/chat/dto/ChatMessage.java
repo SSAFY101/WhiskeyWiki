4da8b1f9639fbf6ed48c3c7354cdb9ab62dcd42f
안윤철 <anssd1@naver.com>
2024-03-20 15:17:36 +0900

    Feat: chat test

--- backend/src/main/java/com/ssafy/whiskeywiki/domain/chat/dto/ChatMessage.java
+++ backend/src/main/java/com/ssafy/whiskeywiki/domain/chat/dto/ChatMessage.java
@@ -9,7 +9,7 @@ public class ChatMessage {
 //        ENTER, TALK
 //    }
 //    private String type; // 메시지 타입
-    private String roomId; // 방번호
+    private String chatroomId; // 방번호
     private String sender; // 메시지 보낸사람
     private String message; // 메시지
 }
