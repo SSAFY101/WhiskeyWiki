5173701a155bd89a78a867dc53fcaa87f5846ad4
임현승 <lhs2689@naver.com>
2024-03-27 11:01:45 +0900

    Chore: change controller logic that change get userId on parameter into get userId on access token

--- backend/src/main/java/com/ssafy/whiskeywiki/domain/cocktail/controller/FavoriteController.java
+++ backend/src/main/java/com/ssafy/whiskeywiki/domain/cocktail/controller/FavoriteController.java
@@ -3,6 +3,7 @@ package com.ssafy.whiskeywiki.domain.cocktail.controller;
 import com.oracle.svm.core.annotate.Delete;
 import com.ssafy.whiskeywiki.domain.cocktail.dto.FavoriteDTO;
 import com.ssafy.whiskeywiki.domain.cocktail.service.FavoriteService;
+import com.ssafy.whiskeywiki.domain.user.service.UserService;
 import com.ssafy.whiskeywiki.global.util.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,12 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+    private final UserService userService;
 
-    //칵테일 즐겨찾기 등록
-    //추후 jwt 토큰 이용시, 헤더에서 userId 꺼내올 예정. 지금은 userId를 파라미터로 받자.
     @PostMapping("/register/{cocktailId}")
-    public ResponseEntity<CommonResponse> addFavorite(int userId, @PathVariable(name = "cocktailId") int cocktailId){
-//        int userId = //헤더의 AccessToken에서 userId 찾기
+    public ResponseEntity<CommonResponse> addFavorite(@RequestHeader(name = "Access-Token") String accessToken, @PathVariable(name = "cocktailId") int cocktailId){
+        int userId = userService.getUserIdByAccessToken(accessToken);
+
         favoriteService.addFavorite(userId, cocktailId);
 
         //userId에 해당하는 favoriteList를 가져와야함
@@ -34,8 +35,9 @@ public class FavoriteController {
 
     //칵테일 즐겨찾기 해제
     @DeleteMapping("/delete/{cocktailId}")
-    public ResponseEntity<CommonResponse> deleteFavorite(int userId, @PathVariable(name = "cocktailId") int cocktailId){
-//      int userId = //헤더의 AccessToken에서 userId 찾기
+    public ResponseEntity<CommonResponse> deleteFavorite(@RequestHeader(name = "Access-Token") String accessToken, @PathVariable(name = "cocktailId") int cocktailId){
+
+        int userId = userService.getUserIdByAccessToken(accessToken);
         favoriteService.deleteFavorite(userId, cocktailId);
 
         return new ResponseEntity<>(CommonResponse.builder()
