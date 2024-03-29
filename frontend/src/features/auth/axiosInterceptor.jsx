848594b1fedb87e4239c54bdac6cc818558863ac
조연주 <yjyk9679@naver.com>
2024-03-29 14:20:17 +0900

    Feat: refreshToken(Cookie) 재발급 설정 완료

--- frontend/src/features/auth/axiosInterceptor.jsx
+++ frontend/src/features/auth/axiosInterceptor.jsx
@@ -1,8 +1,14 @@
 import axios from "axios";
+import { userAction } from "../../store/slices/user";
+
+const { dispatch } = store;
 
 const instance = axios.create({
   baseURL: "",
   timeout: 5000,
+  headers: {
+    withCredentials: true,
+  },
 });
 
 instance.interceptors.response.use(
@@ -10,45 +16,45 @@ instance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    console.log("------- interceptors response error -------"); // test
-    if (error.response.status == 400) {
-      getNewToken();
+    // 새로고침해서 토큰이 없거나, 토큰이 만료된 경우
+    if (error.response.status == 400 || error.response.status == 401) {
+      // 재발급 요청
+      await axios
+        .post("http://localhost:5000/api/auth/refresh", null, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          console.log("2. 토큰 재발급", res);
 
-      const response = await axios.request(error.config);
+          // axios 재설정
+          const accessToken = res.headers["authorization"];
+          instance.defaults.headers.common["Authorization"] = `${accessToken}`;
+          instance.defaults.headers.post["Content-Type"] = "application/json";
+        })
+        .catch((err) => {
+          console.log("토큰 재발급 실패", err);
+          if (err.response.status == 401) {
+            console.log("401");
+            alert("다시 로그인해주세요.");
+            autoLogout();
+          }
+        });
+
+      console.log("3. getNewToken-after");
+
+      const response = await instance.request(error.config);
+
       return response;
     }
     return Promise.reject(error);
   }
 );
 
-const getNewToken = () => {
-  console.log("getNewToken");
-  // 재발급 요청
-  axios
-    .post("http://localhost:5000/api/auth/refresh", null, {
-      withCredentials: true,
-    })
-    .then((res) => {
-      console.log("토큰 재발급", res);
-      const accessToken = res.headers["authorization"];
-
-      instance.defaults.headers.common["Authorization"] = `${accessToken}`;
-      instance.defaults.headers.post["Content-Type"] = "application/json";
-    })
-    .catch((err) => {
-      console.log("토큰 재발급 실패", err);
-      if (err.response.status == 401) {
-        console.log("401");
-        // alert("다시 로그인해주세요.");
-        // autoLogout();
-      }
-    });
-};
-
 const autoLogout = () => {
   // 리프래시 토큰 만료 로그아웃
-  axios.defaults.headers.common["Authorization"] = null;
-  // window.location.reload();
+  instance.defaults.headers.common["Authorization"] = null;
+  dispatch(userAction.setNickname(null));
+  window.location.reload();
 };
 
 export default instance;
