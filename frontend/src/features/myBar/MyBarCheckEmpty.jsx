fae92c41c1900a024b235a4417424ab187d8b0f3
이지은 <lju0514@naver.com>
2024-04-01 22:59:57 +0900

    Feat: 마이바 모달 위스키 보유 상태 관리 API 연동

--- frontend/src/features/myBar/MyBarCheckEmpty.jsx
+++ frontend/src/features/myBar/MyBarCheckEmpty.jsx
@@ -1,17 +1,31 @@
+// import axios from "axios";
+import instance from "../auth/axiosInterceptor";
+
 function MyBarCheckEmpty({ whiskeyId, whiskeyNameKr, whiskeyNameEn }) {
   const checkEmpty = () => {
-    console.log("check Empty");
-    // POST 요청: 위스키 보유 상태 관리 (보유/빈병 변경)
-    // axios({
-    //   method: "post",
-    //   url: "api/mybar/status/{whiskeyId}",
-    // })
-    //   .then((res) => {
-    //     console.log("위스키 상태 변경 성공 : ", res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.log("위스키 상태 변경 ERROR :", err);
-    //   });
+    // GET 요청: 위스키 보유 상태 확인
+    instance({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}/mybar/${whiskeyId}`,
+    })
+      .then((res) => {
+        console.log("위스키 보유 상태 확인 성공 : ", res.data);
+      })
+      .catch((err) => {
+        console.log("위스키 보유 상태 확인 ERROR :", err);
+      });
+
+    // POST 요청: 위스키 보유 상태 관리 (보유 <-> 빈병 변경)
+    instance({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}/mybar/status/${whiskeyId}`,
+    })
+      .then((res) => {
+        // console.log("위스키 상태 변경 성공 : ", res.data);
+      })
+      .catch((err) => {
+        console.log("위스키 상태 변경 ERROR :", err);
+      });
   };
   return (
     <>
