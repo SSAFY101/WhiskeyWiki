d849094a197e597ef05f8ba5b604bcda1536ebe1
안윤철 <anssd1@naver.com>
2024-04-02 00:40:15 +0900

    Feat: object to json, object to image

--- AI/object-detection/main.py
+++ AI/object-detection/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
 )
 
 
-@app.get('/notify/v1/health')
+@app.get('/notify/v1/whiskey')
 def get_health():
     """
     Usage on K8S
@@ -50,21 +50,46 @@ def get_health():
 
 
 @app.post("/object-to-json")
-async def detect_food_return_json_result(file: bytes = File(...)):
+async def detect_whiskey_return_json_result(file: bytes = File(...)):
     input_image = get_image_from_bytes(file)
     results = model(input_image)
-    detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
-    detect_res = json.loads(detect_res)
-    return {"result": detect_res}
+    result_json = results_to_json(results, model)
+    # detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+    # detect_res = json.loads(detect_res)
+    return {"result": result_json}
 
+def results_to_json(results, model):
+    answer = []
+    whiskeys = {}
+    utils = {}
+    othersCount = 0
+    whiskeyCount = 0
+    for result in results.xyxy:
+        for pred in result:
+            class_name = model.model.names[int(pred[5])]
+            if class_name == 'others':
+                othersCount += 1
+                continue
+
+            if class_name in whiskeys:
+                whiskeys[class_name] += 1
+            else:
+                whiskeys[class_name] = 1
+            whiskeyCount += 1
+
+    utils["others"] = othersCount
+    utils["all"] = whiskeyCount + othersCount
+    answer.append(whiskeys)
+    answer.append(utils)
+    return answer
 
 @app.post("/object-to-img")
 async def detect_food_return_base64_img(file: bytes = File(...)):
     input_image = get_image_from_bytes(file)
     results = model(input_image)
-    results.render()  # updates results.imgs with boxes and labels
-    for img in results.imgs:
-        bytes_io = io.BytesIO()
-        img_base64 = Image.fromarray(img)
-        img_base64.save(bytes_io, format="jpeg")
+    r_img = results.render()  # updates results.imgs with boxes and labels
+
+    bytes_io = io.BytesIO()
+    img_base64 = Image.fromarray(r_img[0])
+    img_base64.save(bytes_io, format="jpeg")
     return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
