0f63c7b3b8adfd533b3d5f6232d26b2291977b38
안윤철 <anssd1@naver.com>
2024-04-01 23:30:14 +0900

    Feat: AI first commit

---
+++ AI/yolov5-fastapi-main/segmentation.py
@@ -0,0 +1,26 @@
+import torch
+from PIL import Image
+import io
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+def get_yolov5():
+    # local best.pt
+    model = torch.hub.load('./yolov5', 'custom', path='./model/whiskey_test.pt', source='local', force_reload=True)  # local repo
+    model.conf = 0.5
+    return model
+
+
+def get_image_from_bytes(binary_image, max_size=1024):
+    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize(
+        (
+            int(input_image.width * resize_factor),
+            int(input_image.height * resize_factor),
+        )
+    )
+    return resized_image
