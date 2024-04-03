2463eb930096142b2d9f8b48c92d3967c747a53d
안윤철 <anssd1@naver.com>
2024-04-04 01:40:14 +0900

    Feat: AI 로직 구현

--- backend/src/main/java/com/ssafy/whiskeywiki/domain/chat/domain/Chat.java
+++ backend/src/main/java/com/ssafy/whiskeywiki/domain/chat/domain/Chat.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @Getter
 public class Chat {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "chat_id")
     private int id;
 
