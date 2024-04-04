875541776d336ab7438dde4d59f868e616fdedad
조연주 <yjyk9679@naver.com>
2024-04-04 16:08:13 +0900

    Feat: 발렌타인 세부 모델 인덱스 추가

--- frontend/src/features/WhiskeyImages.jsx
+++ frontend/src/features/WhiskeyImages.jsx
@@ -20,6 +20,18 @@ const WhiskeyImages = [
     imgUrl: require("../assets/images/whiskey/Ballantines_Finest.png"),
   },
   {
+    nameEn: "Ballantines 17",
+    imgUrl: require("../assets/images/whiskey/Ballantines_Finest.png"),
+  },
+  {
+    nameEn: "Ballantines 21",
+    imgUrl: require("../assets/images/whiskey/Ballantines_Finest.png"),
+  },
+  {
+    nameEn: "Ballantines 30",
+    imgUrl: require("../assets/images/whiskey/Ballantines_Finest.png"),
+  },
+  {
     nameEn: "Beefeater",
     imgUrl: require("../assets/images/whiskey/Beefeater.png"),
   },
